ae9a0e4b57c53d359344bc4ae1707fa649797f63
Sntree2mi8 <sntree238@gmail.com>
2024-02-06 00:02:13 +0900

    雑にサーバー立てた

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,17 @@
 module practice_remix_connect/backend
 
 go 1.21
+
+require (
+	github.com/Sntree2mi8/practice_remix_connect/proto/gen/go v0.0.0-20240205145145-13a8fe22ce97
+	google.golang.org/grpc v1.61.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20231106174013-bbf56f31fb17 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
+)
--- backend/main.go
+++ backend/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"github.com/Sntree2mi8/practice_remix_connect/proto/gen/go/v1/hello"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+)
 
+func main() {
+	port := 50051
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	hello.RegisterHelloServiceServer(s, &server{})
+	reflection.Register(s)
+
+	go func() {
+		log.Printf("start gRPC server port: %v", port)
+		s.Serve(listener)
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("stopping gRPC server...")
+	s.GracefulStop()
+}
+
+var _ hello.HelloServiceServer = (*server)(nil)
+
+type server struct {
+}
+
+func (s server) SayHello(ctx context.Context, request *hello.HelloRequest) (*hello.HelloResponse, error) {
+	return &hello.HelloResponse{
+		Message: "Hello, " + request.GetName(),
+	}, nil
 }
